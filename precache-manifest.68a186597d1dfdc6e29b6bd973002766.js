f7df0f7148e08a1bdaeb87661abafce027bce640
superDongE <dlagoehd13@gmail.com>
2020-07-19 23:06:25 +0900

    Updates

---
+++ precache-manifest.68a186597d1dfdc6e29b6bd973002766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e93abf6464ea6eb432ff7a7ab81d6f",
+    "url": "/React-Rest-API/index.html"
+  },
+  {
+    "revision": "d95836b01ad77a066f64",
+    "url": "/React-Rest-API/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "caa0a8f64407a5251b5a",
+    "url": "/React-Rest-API/static/js/2.f2379d0d.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/React-Rest-API/static/js/2.f2379d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95836b01ad77a066f64",
+    "url": "/React-Rest-API/static/js/main.b83810e5.chunk.js"
+  },
+  {
+    "revision": "2a04537da271e7deea55",
+    "url": "/React-Rest-API/static/js/runtime-main.638daf8d.js"
+  }
+]);
