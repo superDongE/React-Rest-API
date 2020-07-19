fd76489e1d9dc13e228b6d1ee89b1d4c5a98d3f3
superDongE <dlagoehd13@gmail.com>
2020-07-19 22:58:54 +0900

    React async

---
+++ src/asyncActionUtil.js
@@ -0,0 +1,77 @@
+import { act } from "react-dom/test-utils";
+
+export default function createAsyncDispatcher(type, promiseFn) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  async function actionHandler(dispatch, ...rest) {
+    dispatch({ type });
+    try {
+      const data = await promiseFn(...rest);
+      dispatch({
+        type: SUCCESS,
+        data,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        error: e,
+      });
+    }
+  }
+  return actionHandler;
+}
+export const initalAsyncState = {
+  loading: false,
+  data: null,
+  error: null,
+};
+
+const loadingState = {
+  loading: true,
+  data: null,
+  error: null,
+};
+
+// 성공했을 때의 상태 만들어주는 함수
+const success = (data) => ({
+  loading: false,
+  data,
+  error: null,
+});
+
+// 실패했을 때의 상태 만들어주는 함수
+const error = (error) => ({
+  loading: false,
+  data: null,
+  error: error,
+});
+
+export function createAsyncHandler(type, key) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  function handler(state, action) {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: loadingState,
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: success(action.data),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: error(action.error),
+        };
+      default:
+        return state;
+    }
+  }
+
+  return handler;
+}
